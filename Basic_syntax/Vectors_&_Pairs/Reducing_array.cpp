aa370bd7af2d34944ddc4cc286d15ba4d8c0c5a4
Rajat Dodwadkar <64971576+RajatDodwadkar@users.noreply.github.com>
2022-03-11 01:02:52 +0530

    Add files via upload

---
+++ Basic_syntax/Vectors_&_Pairs/Reducing_array.cpp
@@ -0,0 +1,14 @@
+// Reducing the array.
+/*Input : 17 9 2 5 1 10 34 (arry)
+Output :  5  3 1 2 0  4  6 (array with position in qascending order eg: smallest value is replaced by 0 and so on)
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int arr[]={10,16,7,14,5,3,2,9}
+    vector <pair><int,int>> v;
+    
+}
